ec3cfd6c23e9c79b2d201cc96d5f9151f27c8d80
ismail2809 <ismaelabounasr@gmail.com>
2026-05-07 23:51:52 +0100

    roles access

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,14 +6,21 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasRoles;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->hasAnyRole(['super-admin', 'admin', 'rh', 'manager', 'comptable']);
+    }
 
     protected $fillable = [
         'name',
